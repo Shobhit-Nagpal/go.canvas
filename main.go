aa37881437cf9363e15e1e027aa38b40065a3d15
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-08-22 10:40:54 +0530

    grid

--- main.go
+++ main.go
@@ -11,16 +11,13 @@ import (
 
 func main() {
 	myApp := app.New()
-	myWindow := myApp.NewWindow("Go canvas")
+	myWindow := myApp.NewWindow("Grid Layout")
 
-	text1 := canvas.NewText("Editor", color.White)
-	text2 := canvas.NewText("Scan", color.White)
-	text3 := canvas.NewText("Infer", color.White)
-	content := container.New(layout.NewVBoxLayout(), layout.NewSpacer(),text1, text2, text3, layout.NewSpacer())
-
-	text4 := canvas.NewText("centered", color.White)
-	centered := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), text4, layout.NewSpacer())
-	myWindow.SetContent(container.New(layout.NewHBoxLayout(), content, layout.NewSpacer(), centered, layout.NewSpacer()))
+	text1 := canvas.NewText("1", color.White)
+	text2 := canvas.NewText("2", color.White)
+	text3 := canvas.NewText("3", color.White)
+	grid := container.New(layout.NewGridLayout(2), text1, text2, text3)
+	myWindow.SetContent(grid)
 	myWindow.ShowAndRun()
 }
 
