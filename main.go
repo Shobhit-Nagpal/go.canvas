c4d6a8d3b32a953834d9dc9361f4a51bbcc5e074
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-08-22 11:24:30 +0530

    tabs

--- main.go
+++ main.go
@@ -3,20 +3,24 @@ package main
 import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/layout"
+	//"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	myApp := app.New()
-	myWindow := myApp.NewWindow("Form Layout")
+	myWindow := myApp.NewWindow("TabContainer Widget")
 
-	label1 := widget.NewLabel("Label 1")
-	value1 := widget.NewLabel("Value")
-	label2 := widget.NewLabel("Label 2")
-	value2 := widget.NewLabel("Something")
-	grid := container.New(layout.NewFormLayout(), label1, value1, label2, value2)
-	myWindow.SetContent(grid)
+	tabs := container.NewAppTabs(
+		container.NewTabItem("Tab 1", widget.NewLabel("Hello")),
+		container.NewTabItem("Tab 2", widget.NewLabel("World!")),
+	)
+
+	//tabs.Append(container.NewTabItemWithIcon("Home", theme.HomeIcon(), widget.NewLabel("Home tab")))
+
+	tabs.SetTabLocation(container.TabLocationLeading)
+
+	myWindow.SetContent(tabs)
 	myWindow.ShowAndRun()
 }
 
